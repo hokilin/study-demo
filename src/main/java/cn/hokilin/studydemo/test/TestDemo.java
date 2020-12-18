9f865ad17c69385efd58d4e41e4aab45b354b9fc
hokilin <hoki.lin@esmartwave.com>
2020-12-18 11:09:23 +0800

    添加判断日期合法工具方法

--- src/main/java/cn/hokilin/studydemo/test/TestDemo.java
+++ src/main/java/cn/hokilin/studydemo/test/TestDemo.java
@@ -1,7 +1,13 @@
 package cn.hokilin.studydemo.test;
 
 
-import org.apache.commons.codec.digest.DigestUtils;
+import cn.hutool.core.date.DateUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * @author linhuankai
@@ -9,11 +15,45 @@ import org.apache.commons.codec.digest.DigestUtils;
  */
 public class TestDemo {
     public static void main(String[] args) {
-        String encryptedPhone = DigestUtils.md5Hex("123456" + "123456" + "15222892752" + "123456");
-        encryptedPhone = DigestUtils.md5Hex(encryptedPhone.toUpperCase()).toUpperCase();
-        System.out.println(encryptedPhone);
+//        String encryptedPhone = DigestUtils.md5Hex("123456" + "123456" + "15222892752" + "123456");
+//        encryptedPhone = DigestUtils.md5Hex(encryptedPhone.toUpperCase()).toUpperCase();
+//        System.out.println(encryptedPhone);
+//
+//        String encryptedPhone1 = DigestUtils.md5Hex(DigestUtils.md5Hex(("tmall" + "15222892752" + "123456")));
+//        System.out.println(encryptedPhone1);
 
-        String encryptedPhone1 = DigestUtils.md5Hex(DigestUtils.md5Hex(("tmall" + "15222892752" + "123456")));
-        System.out.println(encryptedPhone1);
+//        Date date = DateUtil.parse("1995-03-02 02:02:02", "yyyy-MM-dd");
+//        System.out.println(date);
+
+        System.out.println(isLegalDateStr("2020-02-29"));
+        System.out.println(isLegalDateTimeStr("2020-02-28 00:00:01"));
+    }
+
+    public static boolean isLegalDateStr(String dateStr) {
+        String patten = "yyyy-MM-dd";
+        if (StringUtils.isEmpty(dateStr) || StringUtils.isEmpty(patten) || dateStr.length() != patten.length()) {
+            return false;
+        }
+        try {
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(patten);
+            LocalDate localDate = LocalDate.parse(dateStr, dateTimeFormatter);
+            return dateStr.equals(localDate.format(dateTimeFormatter));
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isLegalDateTimeStr(String dateStr) {
+        String patten = "yyyy-MM-dd HH:mm:ss";
+        if (StringUtils.isEmpty(dateStr) || StringUtils.isEmpty(patten) || dateStr.length() != patten.length()) {
+            return false;
+        }
+        try {
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(patten);
+            LocalDateTime dateTime = LocalDateTime.parse(dateStr, dateTimeFormatter);
+            return dateStr.equals(dateTime.format(dateTimeFormatter));
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
