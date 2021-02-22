81a56ab53544657ae3342f9538f8a459bb8a0b6f
hokilin <hoki.lin@esmartwave.com>
2021-02-22 10:23:18 +0800

    调整动态代理代码位置

---
+++ src/main/java/cn/hokilin/studydemo/designmode/dynamicproxy/BookApiImpl.java
@@ -0,0 +1,11 @@
+package cn.hokilin.studydemo.designmode.dynamicproxy;
+
+/**
+ * @author linhuankai
+ * @date 2021/2/1 22:20
+ */
+public class BookApiImpl implements BookApi {
+    @Override
+    public void sell() {
+    }
+}
